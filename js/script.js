ec02b867a7bcd76325634092ec389773a3bdee7d
Lucas Peixer <lojapeixer@gmail.com>
2022-10-16 18:17:05 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const formulario = document.querySelector('#form1');
+const listaTarefa = [];
+
+const inputAtividade = document.getElementById('atividade')
+
+formulario.addEventListener("submit", evento =>{
+    evento.preventDefault();
+
+    const cadastroAtividade = criarDadosDaAtividade();
+
+    if (cadastroAtividade) {
+        gravarAtividade(cadastroAtividade);
+    }
+
+    if (!cadastroAtividade) {
+        alert('Favor preencher o campo!');
+    }
+    
+    formulario.reset();
+    inputAtividade.focus();
+
+    console.log (listaTarefa);
+});
+
+function criarDadosDaAtividade() {
+    const titulo = inputAtividade.value;
+    const atividade = {titulo, status: 'uncompleted'}
+
+    if (titulo) {return atividade}
+
+    return null;    
+}
+
+function gravarAtividade(cadastroAtividade){
+     listaTarefa.push(cadastroAtividade);
+}
+
+
+const localizar = document.getElementById('opc')
+localizar.addEventListener("change", componente => {
+    let filtered = [];
+    switch (componente.target.value){
+        case "all" : filtered = listaTarefa; console.log(filtered); break;
+        case "uncompleted" : filtered = listaTarefa; console.log(filtered); break;
+        default : console.log('Nenhuma Finalizada!');
+    }
+});
