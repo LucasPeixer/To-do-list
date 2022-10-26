270cb392845771968f3bb9e796085febacca9a45
LucasPeixer <lucaspeicher@hotmail.com>
2022-10-26 19:57:51 -0300

    Atualização

---
+++ js/login.js
@@ -0,0 +1,18 @@
+const localizarLogin = document.querySelector('#login');
+const usuario_esperado = 'admin';
+const senha_esperado = '1234';
+
+localizarLogin.addEventListener("submit", eventoLogin => {
+    eventoLogin.preventDefault();
+
+    const login = document.querySelector('#usuario').value;
+    const password = document.querySelector('#senha').value;
+
+    if (login === usuario_esperado && password === senha_esperado){
+        window.sessionStorage.setItem('ConfirmacaoLogin','true');
+        window.location = 'index.html';
+    }else{
+        window.sessionStorage.setItem('ConfirmacaoLogin','false');
+        alert('Usuário ou senha incorreto');
+    }
+});
